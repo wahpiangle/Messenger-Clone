ceb79d7bb45a84744e5b4cbef10f3973e0907f84
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-05-23 21:03:32 +0800

    empty page on no message

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,12 +1,9 @@
-'use client'
-
-import { signOut } from 'next-auth/react'
-import React from 'react'
+import EmptyState from '../components/EmptyState'
 
 const Users = () => {
   return (
     <div className='hidden lg:block lg:pl-80 h-full'>
-
+      <EmptyState/>
     </div>
   )
 }
